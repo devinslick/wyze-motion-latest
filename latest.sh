5a35a7695ebf5fac235ce0f7c6500a4df7bb18b9
Devin Slick <devinslick@users.noreply.github.com>
2022-10-08 20:07:48 -0500

    Rewrite to use cp instead of mount
    Checking inodes and using bind mounts wasn't working well.  This is more reliable.

--- latest.sh
+++ latest.sh
@@ -10,6 +10,9 @@
 # Example file contents:
 #alarmWebhookURL=https://subdomain.domain.com/api/webhook/my-secret-camera-alarm-url
 #videoWebhookURL=https://subdomain.domain.com/api/webhook/my-secret-camera-alarm-url
+#
+# To access the last events and videos use http://camera-address/latest.jpg and http://camera-address/latest.mp4
+#
 
 #import variables for webhook notifications to home assistant
 if [ -f /media/mmc/wz_mini/.env ]; then
@@ -17,50 +20,60 @@ if [ -f /media/mmc/wz_mini/.env ]; then
     export $(grep -v '^#' /media/mmc/wz_mini/.env | xargs)
 fi
 
-#create 'latest.jpg' and 'latest.mp4' files if they don't exist yet
-if [ ! -f /media/mmc/wz_mini/www/latest.jpg ]; then
-    echo "latest.jpg not found, creating placeholder for mount with touch..."
-    touch /media/mmc/wz_mini/www/latest.jpg
-fi
-if [ ! -f /media/mmc/wz_mini/www/latest.mp4 ]; then
-    echo "latest.mp4 not found, creating placeholder for mount with touch..."
-    touch /media/mmc/wz_mini/www/latest.mp4
-fi
+#get newest available alarm jpg and mp4 video (inode and path)
+pathNewestJPG=$(ls $(find /media/mmc/alarm/$(ls /media/mmc/alarm/ | tail -1)/ -name "*.jpg" -print | tail -1))
+pathNewestMP4=$(ls $(find /media/mmc/record/ -name "*.mp4" -print | tail -1))
 
-#get the inode for latest.jpg, latest.mp4, the most recent motion jpg, and the most recent motion mp4
-currentAlarmFile=$(ls -i /media/mmc/wz_mini/www/latest.jpg)
-currentVideoFile=$(ls -i /media/mmc/wz_mini/www/latest.mp4)
-newAlarmFile=$(ls -i $(find /media/mmc/alarm/$(ls /media/mmc/alarm/ | tail -1)/ -name "*.jpg" -print | tail -1))
-newVideoFile=$(ls -i $(find /media/mmc/record/ -name "*.mp4" -print | tail -1))
-cINodeAlarm=$(echo $currentAlarmFile | cut -d ' ' -f 1)
-nINodeAlarm=$(echo $newAlarmFile | cut -d ' ' -f 1)
-cINodeVideo=$(echo $currentVideoFile | cut -d ' ' -f 1)
-nINodeVideo=$(echo $newVideoFile | cut -d ' ' -f 1)
-
-#compare the inodes to determine if the bind mounts need to be updated
-if [ $cINodeAlarm == $nINodeAlarm ]
-then
-      echo "Alarm iNodes are the same ($cINodeAlarm), file has not changed"
+#read the last jpg filename from disk into a var
+if [ -f /media/mmc/wz_mini/lastMotionFileJPG ]; then
+    lastJPGCopied=$(cat /media/mmc/wz_mini/lastMotionFileJPG)
+    if [ $lastJPGCopied == $pathNewestJPG ]
+    then
+        echo "jpg files are the same"
+    else
+        echo "new jpg needs to be copied"
+        cp -f $pathNewestJPG /media/mmc/wz_mini/www/latest.jpg
+        echo $pathNewestJPG > /media/mmc/wz_mini/lastMotionFileJPG
+        if test ${alarmWebhookURL}
+        then
+            echo "Notifying Home Assistant of the new alarm event..."
+            curl -kX POST -H "Content-Type: application/json" -d '{ "path": "$pathNewestJPG" }' $alarmWebhookURL
+        fi
+    fi
 else
-    newPath=$(echo $newAlarmFile | cut -d ' ' -f 2)
-    echo "Alarm iNodes are different ($cINodeAlarm vs $nINodeAlarm); file has changed. Mount being updated to $newPath"
-    /bin/mount --bind $newPath /opt/wz_mini/www/latest.jpg
+    echo "New! new file needs to be copied"
+    cp $pathNewestJPG /media/mmc/wz_mini/www/latest.jpg
+    echo $pathNewestJPG > /media/mmc/wz_mini/lastMotionFileJPG
     if test ${alarmWebhookURL}
     then
         echo "Notifying Home Assistant of the new alarm event..."
-        curl -kX POST -d '{ "path": "$newPath" }' $alarmWebhookURL
+        curl -kX POST -H "Content-Type: application/json" -d '{ "path": "$pathNewestJPG" }' $alarmWebhookURL
     fi
 fi
-if [ $cINodeVideo == $nINodeVideo ]
-then
-    echo "Video iNodes are the same ($cINodeVideo), file has not changed"
+
+#read the last mp4 filename from disk into a var
+if [ -f /media/mmc/wz_mini/lastVideoFileMP4 ]; then
+    lastMP4Copied=$(cat /media/mmc/wz_mini/lastVideoFileMP4)
+    if [ $lastMP4Copied == $pathNewestMP4 ]
+    then
+        echo "mp4 files are the same"
+    else
+        echo "new mp4 needs to be copied"
+        cp -f $pathNewestMP4 /media/mmc/wz_mini/www/latest.mp4
+        echo $pathNewestMP4 > /media/mmc/wz_mini/lastVideoFileMP4
+        if test ${videoWebhookURL}
+        then
+            echo "Notifying Home Assistant of the new video event..."
+            curl -kX POST -H "Content-Type: application/json" -d '{ "path": "$pathNewestMP4" }' $videoWebhookURL
+        fi
+    fi
 else
-    newPath=$(echo $newVideoFile | cut -d ' ' -f 2)
-    echo "Video iNodes are different ($cINodeAlarm vs $nINodeAlarm); file has changed. Mount being updated to $newPath"
-    /bin/mount --bind $newPath /opt/wz_mini/www/latest.mp4
+    echo "New! new file needs to be copied"
+    cp $pathNewestMP4 /media/mmc/wz_mini/www/latest.mp4
+    echo $pathNewestMP4 > /media/mmc/wz_mini/lastVideoFileMP4
     if test ${videoWebhookURL}
     then
         echo "Notifying Home Assistant of the new video event..."
-        curl -kX POST -d '{ "path": "$newPath" }' $videoWebhookURL
+        curl -kX POST -H "Content-Type: application/json" -d '{ "path": "$pathNewestMP4" }' $videoWebhookURL
     fi
 fi
