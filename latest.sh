669acff0bfaaa74c324f1c93027b86560d3d3a31
Devin Slick <devinslick@users.noreply.github.com>
2022-10-09 10:44:12 -0500

    Use wz_mini.conf for settings

--- latest.sh
+++ latest.sh
@@ -6,19 +6,18 @@
 #* * * * * /media/mmc/wz_mini/latest.sh
 # to this file: /media/mmc/wz_mini/etc/cron/root
 #
-# To use webhooks create a configuration file at this path: /media/mmc/wz_mini/.env
-# Example file contents:
-#alarmWebhookURL=https://subdomain.domain.com/api/webhook/my-secret-camera-alarm-url
-#videoWebhookURL=https://subdomain.domain.com/api/webhook/my-secret-camera-alarm-url
 #
 # To access the last events and videos use http://camera-address/latest.jpg and http://camera-address/latest.mp4
 #
+# To use webhooks you will need ALARM_WEBHOOK and/or VIDEO_WEBHOOK defined in /media/mmc/wz_mini/wz_mini.conf
+# As an example, you may append the following to the bottom of the file:
+###### LATEST #####
+#ALARM_WEBHOOK="https://subdomain.domain.com/api/webhook/my-secret-camera-alarm-url"
+#VIDEO_WEBHOOK="https://subdomain.domain.com/api/webhook/my-secret-camera-alarm-url"
 
 #import variables for webhook notifications to home assistant
-if [ -f /media/mmc/wz_mini/.env ]; then
-    grep -v '^#' /media/mmc/wz_mini/.env
-    export $(grep -v '^#' /media/mmc/wz_mini/.env | xargs)
-fi
+alarmWebhookURL=cat /media/mmc/wz_mini/wz_mini.conf | grep ALARM_WEBHOOK | cut -d '=' -f 2 | sed 's/"//g'
+videoWebhookURL=cat /media/mmc/wz_mini/wz_mini.conf | grep VIDEO_WEBHOOK | cut -d '=' -f 2 | sed 's/"//g'
 
 #get newest available alarm jpg and mp4 video (inode and path)
 pathNewestJPG=$(ls $(find /media/mmc/alarm/$(ls /media/mmc/alarm/ | tail -1)/ -name "*.jpg" -print | tail -1))
@@ -29,24 +28,24 @@ if [ -f /media/mmc/wz_mini/lastMotionFileJPG ]; then
     lastJPGCopied=$(cat /media/mmc/wz_mini/lastMotionFileJPG)
     if [ $lastJPGCopied == $pathNewestJPG ]
     then
-        echo "jpg files are the same"
+        echo "The last created jpg file was already copied to latest.jpg.  Skipping."
     else
-        echo "new jpg needs to be copied"
+        echo "New JPG! Copying $pathNewestJPG to latest.jpg..."
         cp -f $pathNewestJPG /media/mmc/wz_mini/www/latest.jpg
         echo $pathNewestJPG > /media/mmc/wz_mini/lastMotionFileJPG
         if test ${alarmWebhookURL}
         then
-            echo "Notifying Home Assistant of the new alarm event..."
+            echo "Triggering webhook for new alarm JPG"
             curl -kX POST -H "Content-Type: application/json" -d '{ "path": "$pathNewestJPG" }' $alarmWebhookURL
         fi
     fi
 else
-    echo "New! new file needs to be copied"
+    echo "New JPG! Copying $pathNewestJPG to latest.jpg..."
     cp $pathNewestJPG /media/mmc/wz_mini/www/latest.jpg
     echo $pathNewestJPG > /media/mmc/wz_mini/lastMotionFileJPG
     if test ${alarmWebhookURL}
     then
-        echo "Notifying Home Assistant of the new alarm event..."
+        echo "Triggering webhook for new mp4 video creation"
         curl -kX POST -H "Content-Type: application/json" -d '{ "path": "$pathNewestJPG" }' $alarmWebhookURL
     fi
 fi
@@ -56,24 +55,24 @@ if [ -f /media/mmc/wz_mini/lastVideoFileMP4 ]; then
     lastMP4Copied=$(cat /media/mmc/wz_mini/lastVideoFileMP4)
     if [ $lastMP4Copied == $pathNewestMP4 ]
     then
-        echo "mp4 files are the same"
+        echo "The last created mp4 file was already copied to latest.mp4.  Skipping."
     else
-        echo "new mp4 needs to be copied"
+        echo "New MP4! Copying $pathNewestJPG to latest.mp4..."
         cp -f $pathNewestMP4 /media/mmc/wz_mini/www/latest.mp4
         echo $pathNewestMP4 > /media/mmc/wz_mini/lastVideoFileMP4
         if test ${videoWebhookURL}
         then
-            echo "Notifying Home Assistant of the new video event..."
+            echo "Triggering webhook for new mp4 video creation"
             curl -kX POST -H "Content-Type: application/json" -d '{ "path": "$pathNewestMP4" }' $videoWebhookURL
         fi
     fi
 else
-    echo "New! new file needs to be copied"
+    echo "New MP4! Copying $pathNewestJPG to latest.mp4..."
     cp $pathNewestMP4 /media/mmc/wz_mini/www/latest.mp4
     echo $pathNewestMP4 > /media/mmc/wz_mini/lastVideoFileMP4
     if test ${videoWebhookURL}
     then
-        echo "Notifying Home Assistant of the new video event..."
+        echo "Triggering webhook for new mp4 video creation"
         curl -kX POST -H "Content-Type: application/json" -d '{ "path": "$pathNewestMP4" }' $videoWebhookURL
     fi
 fi
